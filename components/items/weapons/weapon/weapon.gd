6821ebc1120047049bb55e8cb1369d720c4977c9
programmer2005 <dennaumol05@gmail.com>
2025-02-28 17:35:39 +0300

    asdasdads

--- components/items/weapons/weapon/weapon.gd
+++ components/items/weapons/weapon/weapon.gd
@@ -3,6 +3,7 @@ class_name Weapon
 
 @export var projectile_scene: PackedScene
 @export var attack_speed: float = 0.2
+@export var melee_atack_scene: PackedScene
 var attack_speed_timer: float = 0
 var muzzle_component: Muzzle = null
 
@@ -10,11 +11,14 @@ var muzzle_component: Muzzle = null
 func use():
 	if attack_speed_timer >= attack_speed and muzzle_component:
 		attack_speed_timer = 0
-		var projectile_instance = projectile_scene.instantiate()
-		Utilities.get_level().add_child(projectile_instance)
-		projectile_instance.global_position = muzzle_component.global_position
-		projectile_instance.global_rotation = muzzle_component.global_rotation
-		Utilities.find_nodes_by_class_name(projectile_instance, ProjectileMovement)[0].direction = Vector2(cos(muzzle_component.global_rotation), sin(muzzle_component.global_rotation)) 
+		if projectile_scene:
+			var projectile_instance = projectile_scene.instantiate()
+			Utilities.get_level().add_child(projectile_instance)
+			projectile_instance.global_position = muzzle_component.global_position
+			projectile_instance.global_rotation = muzzle_component.global_rotation
+			Utilities.find_node_by_class_name(projectile_instance, ProjectileMovement).direction = Vector2(cos(muzzle_component.global_rotation), sin(muzzle_component.global_rotation))
+		if melee_atack_scene:
+			pass 
 
 func _process(delta: float) -> void:
 	attack_speed_timer += delta
